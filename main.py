6782383299bfd1f885669f65050adcecb15dc4ee
Ehimenose Aigbe <ehialfaigbe@gmail.com>
2025-08-10 00:37:56 -0400

    General Page for air and light sensorcomplete

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def read_root():
 @app.get("/temperature")
 def home_temperature():
     return {"temperature": 22.5, "unit": "Celsius", "timestamp": "2025-08-09T12:00:00Z", "device_id": "esp32-001"}
+
+
+@app.get("/air")
+def home_air_quality():
+    return {"air_quality": "Good", "pm2_5": 12, "pm10": 20, "timestamp": "2025-08-09T12:00:00Z", "device_id": "esp32-001"}
+
+@app.get("/light")
+def home_light():
+    return {"light_intensity": 300, "unit": "lux", "timestamp": "2025-08-09T12:00:00Z", "device_id": "esp32-001"}
