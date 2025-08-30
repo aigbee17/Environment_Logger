9fa00d8a3cc4c34c32532fa92b4b3819489c6828
Ehimenose Aigbe <ehialfaigbe@gmail.com>
2025-08-29 22:24:53 -0400

    Added Input Sensor Readers.

--- Sensor/FirstSensor/main/FirstSensor.c
+++ Sensor/FirstSensor/main/FirstSensor.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
+
+
+
+uint8_t Temp_reader;
+uint8_t Hum_reader;
+uint8_t Air_reader;
+
+
+
+
+
+
+
+
+
 
 void app_main(void)
 {
-
+Temp_reader = 0XFF;
+Hum_reader = 0XFF;
+Air_reader = 0XFF;
 }
 
-int main(void){
-    printf("Hello, World!\n"); //test for sensor
-    return 0;
-}
